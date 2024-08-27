2b0c512885739c91e75737832e099b421def5d22
Maria Lukowich <mlukowich27@gmail.com>
2024-08-27 16:57:38 -0600

    adding poem

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ const poems = [
     poem: "Roses are red \n Violets are blue \n Jerry Seinfeld is considering \n Making Bee Movie 2",
     image: "bee.jpeg",
   },  
+  {
+    author: "Maria",
+    poem: "Roses are red \n Violets are blue \n Jerry Seinfeld is considering \n Making Bee Movie 2",
+    image: "book.jpg",
+  },
 ];
 
 let displayPoems = (array) => {
